288f063e4b5b9f3f83b19f14d451a6860c69912d
KumarGudipalli <kumargudipalli97@gmail.com>
2025-09-24 23:32:32 +0530

    added multiple urls to fix cors origin

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectToDb = async () => {
-  await mongoose.connect(process.env.MONGODB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(process.env.MONGODB);
 };
 
 module.exports =   connectToDb
